a4c9428073209fe7edddc8a93d1228e935fc9572
ZakiSF <yasirmiri17@gmail.com>
2021-09-11 11:44:17 -0700

    Salam

--- src/test/java/gitPractice/FisrtClass.java
+++ src/test/java/gitPractice/FisrtClass.java
@@ -5,6 +5,7 @@ public class FisrtClass {
     public static void main(String[] args) {
 
         System.out.println("this is first");
+        System.out.println("idfid");
 
         System.out.println("");
     }
